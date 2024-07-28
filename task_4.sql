f55d5255b2794e6672d82779237cf97d90f33cb1
DonXess <loklari113@gmail.com>
2024-07-28 21:51:06 +0100

    script task_4

--- task_4.sql
+++ task_4.sql
@@ -1,3 +1,4 @@
-USE alx_book_store;
-SELECT *
-FROM books
+-- Print the full description of the table 'Books' from the 'alx_book_store' database
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'Books';
